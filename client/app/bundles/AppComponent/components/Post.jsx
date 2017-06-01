6c2f470b7bc55b5c815dd58a8b5f2aeaef145d82
Shadid12 <shadid.haque@uoit.net>
2017-06-01 04:44:56 +0000

    react_on_rails

---
+++ client/app/bundles/AppComponent/components/Post.jsx
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export default class Post {
+    render(){
+        return(
+            <h1>This is looking good mate</h1>
+            
+        )
+    }
+}
